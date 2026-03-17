bd7c07c3b1bbf27bac409342e2f409f149b06612
Meganathan N <megacode26@gmail.com>
2026-03-17 14:04:29 +0530

    Tailwind CSS Configuration

---
+++ app/movie/[id].tsx
@@ -0,0 +1,11 @@
+import { useLocalSearchParams } from "expo-router";
+import { Text, View } from "react-native";
+
+export default function Details() {
+  const { id } = useLocalSearchParams();
+  return (
+    <View className="flex-1 justify-center items-center">
+      <Text className="text-5xl text-accent">Movie Name : {id}</Text>
+    </View>
+  );
+}
