2336595b48eb60c84a963cb806c0beafbc85377a
Meganathan N <megacode26@gmail.com>
2026-03-21 10:18:19 +0530

    All Movies Completed

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,65 @@
+import MovieCard from "@/components/MovieCard";
+import SearchBar from "@/components/SearchBar";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { fetchPopularMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { useRouter } from "expo-router";
+import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
+
+export default function Index() {
+  const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchPopularMovies({ query: "" }));
+
+  return (
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <View className="flex-1 px-5">
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+
+        <SearchBar
+          onPress={() => router.push("/search")}
+          placeholder="Search for a Movie"
+        />
+        <Text className="text-lg text-white font-bold mt-5 mb-3">
+          Latest Movies
+        </Text>
+
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#000fff"
+            className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text className="text-white mt-10 text-center">
+            Error: {moviesError?.message}
+          </Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <FlatList
+              data={movies}
+              keyExtractor={(item) => item.id.toString()}
+              renderItem={({ item }) => (
+                // <Text className="text-white text-sm">{item.title}</Text>
+                <MovieCard {...item} />
+              )}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 20,
+                paddingRight: 5,
+                marginBottom: 10,
+              }}
+              className="mt-2 pb-32"
+            />
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
---
+++ components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { icons } from "@/constants/icons";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  placeholder: string;
+  onPress?: () => void;
+}
+
+export default function SearchBar({ placeholder, onPress }: Props) {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full py-4 px-5">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        value=""
+        onChangeText={() => {}}
+        placeholderTextColor="#a8b5db"
+        className="flex-1 ml-2 text-white"
+      />
+    </View>
+  );
+}
