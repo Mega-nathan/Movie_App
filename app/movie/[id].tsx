329921ea3b9fad207261ff1ce2c3579933f12b21
Meganathan N <megacode26@gmail.com>
2026-03-26 21:03:53 +0530

    Detailed Movie Page done

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,11 +1,96 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { fetchMovieDetails } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import { router, useLocalSearchParams } from "expo-router";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+
+interface MovieInfoProps {
+  label: string;
+  value?: string | number | null;
+}
+
+const MovieInfo = ({ label, value }: MovieInfoProps) => (
+  <View className="mt-5">
+    <Text className="text-light-200 font-normal text-sm">{label}</Text>
+    <View className="flex-col items-center justify-center">
+      <Text className="text-light-100 font-bold text-xs">
+        {value || " N/A "}
+      </Text>
+    </View>
+  </View>
+);
 
 export default function Details() {
   const { id } = useLocalSearchParams();
+  const { data: movie, loading } = useFetch(() =>
+    fetchMovieDetails(id as string),
+  );
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-5xl text-accent">Movie Name : {id}</Text>
+    <View className="flex-1 bg-primary">
+      <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
+        <View>
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w500${movie?.poster_path}`,
+            }}
+            className="w-full h-[450px]"
+            resizeMode="stretch"
+          />
+        </View>
+
+        <View className="flex-col items-start justify-center mt-5 px-5">
+          <Text className="text-white font-bold text-xl">{movie?.title}</Text>
+
+          <View className="flex-row items-center gap-x-0 mt-2">
+            <Text className="text-light-200 text-sm">
+              {movie?.release_date?.split("-")[0]}
+              <Text className="text-light-200 text-sm mx-2 mt-2">
+                {movie?.runtime}m
+              </Text>
+            </Text>
+          </View>
+
+          <View className="flex-row items-center bg-dark-100 px-2 py-1 rounded-md gap-x-1 mt-2">
+            <Image source={icons.star} className="size-4" />
+            <Text className="text-white font-bold text-sm">
+              {Math.round(movie?.vote_average ?? 0)}/10
+            </Text>
+            <Text className="text-light-200 text-sm">
+              ({movie?.vote_count} votes)
+            </Text>
+          </View>
+
+          <MovieInfo label="Overview" value={movie?.overview} />
+          <MovieInfo
+            label="Genres"
+            value={movie?.genres?.map((g) => g.name).join(" - ") || "N/A"}
+          />
+
+          <View className="flex flex-row justify-between w-1/2">
+            {/* <MovieInfo label="Budget" value={`$${movie?.budget / 1_000_000 } million`}
+            <MovieInfo label="Revenue" value={`$${movie?.budget / 1_000_000 } million`} */}
+            <MovieInfo
+              label="Production Companies"
+              value={
+                movie?.production_companies.map((c) => c.name).join(" - ") ||
+                "N/A"
+              }
+            />
+          </View>
+        </View>
+      </ScrollView>
+
+      <TouchableOpacity
+        className="absolute bottom-5 left-0 right-0 mx-5 bg-accent rounded-lg py-2.5 flex flex-row items-center justify-center z-50"
+        onPress={router.back}
+      >
+        <Image
+          source={icons.arrow}
+          className="size-5 mr-1 mt-0.5 rotate-180"
+          tintColor="#fff"
+        />
+        <Text className="text-white font-semibold text-base">Go Back</Text>
+      </TouchableOpacity>
     </View>
   );
 }
--- services/api.ts
+++ services/api.ts
@@ -53,6 +53,28 @@ export const fetchPopularTeluguMovies = async ({
   return data.results;
 };
 
+export const fetchMovieDetails = async (
+  movieID: string,
+): Promise<MovieDetails> => {
+  try {
+    const response = await fetch(
+      `${TMDB_CONFIG.BASE_URL}/movie/${movieID}?api_key=${TMDB_CONFIG.API_KEY}`,
+      {
+        method: "GET",
+        headers: TMDB_CONFIG.headers,
+      },
+    );
+
+    if (!response.ok) throw new Error("Failed to fetch movie details");
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
 // const url = "https://api.themoviedb.org/3/trending/movie/day";
 // const options = {
 //   method: "GET",
