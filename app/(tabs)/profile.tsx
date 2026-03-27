735a310c1039b4e714f04ca47e6e4ae7b0203c89
Meganathan N <megacode26@gmail.com>
2026-03-27 23:50:40 +0530

    saved file change

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,18 +1,14 @@
 import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
 import React from "react";
-import { Image, ScrollView, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 export default function Saved() {
   return (
-    <ScrollView className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full z-0" />
-      <View className="flex-1 px-5">
-        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
-        {/* <SearchBar onPress={() => {}} placeholder="Search for a Movie" /> */}
-        <Text className="text-lg text-white font-bold mt-5 mb-3">Profile</Text>
+    <View className="bg-primary flex-1 px-10">
+      <View className="flex justify-center items-center flex-1 flex-col gap-5">
+        <Image source={icons.person} className="size-10" tintColor="white" />
+        <Text className="text-gray-500 text-base">Profile</Text>
       </View>
-    </ScrollView>
+    </View>
   );
 }
